12fc3409c194edf9e5ac261556a8c175cb36e527
Yifan Ruan <yfruan20@fudan.edu.cn>
2021-05-27 20:45:53 +0800

    Add player.h

--- player.h
+++ player.h
@@ -1,6 +1,29 @@
 #ifndef PLAYER_H_
 #define PLAYER_H_
 
-class Player {};
+#include <string>
+#include <utility>
+
+class Player {
+  public:
+    Player() : name_(""), point_(-1) {}
+
+    explicit Player(std::string name) : name_(std::move(name)), point_(0) {}
+
+    Player(std::string name, int point)
+        : name_(std::move(name)), point_(point) {}
+
+    void AddPoint(int p) { point_ += p; }
+
+    inline std::string name() const { return name_; }
+
+    inline int point() const { return point_; }
+
+    inline void SetPoint(int point) { point_ = point; }
+
+  private:
+    std::string name_;
+    int point_;
+};
 
 #endif // PLAYER_H_
